6b3ff571e36d3cacf0c796bed9ca332c744baf58
Jorge Ossa <jorgehossa@gmail.com>
2024-03-23 11:56:18 -0500

    feat(home): 🎨 add home layout
    this add basic configuration for home layout
    
    Fix: WEA-7

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const WeatherCard = () => {
+  return (
+    <View>
+      <Text>Card</Text>
+    </View>
+  );
+};
+
+const Header = () => {
+  return (
+    <View>
+      <Text>Header</Text>
+    </View>
+  );
+};
+
+const Forecast = () => {
+  return (
+    <View>
+      <Text>Forecast</Text>
+    </View>
+  );
+};
 
 const Home = () => {
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Header />
+      </View>
+      <View style={styles.weatherContainer}>
+        <WeatherCard />
+      </View>
+      <View style={styles.forecastContainer}>
+        <Forecast />
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -14,7 +47,22 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+  },
+  headerContainer: {
+    width: '100%',
+    backgroundColor: 'blue',
+    height: 60,
+  },
+  weatherContainer: {
+    width: '100%',
+    flex: 3,
+    backgroundColor: 'green',
+  },
+  forecastContainer: {
+    width: '100%',
+    backgroundColor: 'purple',
+    marginBottom: Platform.OS === 'ios' ? 90 : 120,
   },
 });
 
